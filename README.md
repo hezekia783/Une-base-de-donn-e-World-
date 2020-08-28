8909f92b73aecac5e606a43e4e545d535073419b
hezekia783 <67896318+hezekia783@users.noreply.github.com>
2020-08-28 22:01:26 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+Dans le cadre de notre formation chez SAYNA 
+##projet aui s'intitule "Une base de donnée World"
+
+==>l'exercice consite a créer une base de données qui s'appelle "World"
+avec la langage SQL
+
+>hezekia
---
+++ Une base de donnée World.sql
@@ -0,0 +1,41 @@
+CREATE TABLE Word (
+id INTEGER,
+nom TEXT,
+prenom TEXT,
+age INTEGER,
+taille REAL,
+poids REAL
+);
+INSERT INTO Word (id, nom, prenom, age, taille, poids)
+VALUES ( 1, "Rakoto", "Be Nify", 10, 1,50, 70,0);
+VALUES ( 2, "Lava", "Rapeto", 25, 2,0, 30,0);
+VALUES ( 3, "Ba", "Lita", 7, 1,0, 20,5);
+VALUES ( 4, "Kiala", "Manjakely", 100, 1,68, 45,7);
+VALUES ( 5, "Kiala", "Pota", 37, 0,8, 50,0);
+
+ALTER TABLE word
+ADD COLUMN couleur_preferee TEXT;
+
+UPDATE word 
+SET couleur_preferee = rouge
+WHERE id =1 ;
+UPDATE word 
+SET couleur_preferee = vert
+WHERE id = 2;
+UPDATE word 
+SET couleur_preferee = jaune
+WHERE id = 3;
+UPDATE word 
+SET couleur_preferee = violet
+WHERE id = 4;
+UPDATE word 
+SET couleur_preferee = grise
+WHERE id = 5;
+
+UPDATE word 
+SET nom = Beorona 
+WHERE id = 3;
+
+UPDATE word 
+SET prenom = Balita 
+WHERE id = 3;
